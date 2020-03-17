95bae83a7b82cfbd017f4012378f95c213c37df0
DELL <wschxida@gmail.com>
2020-03-18 00:20:46 +0800

    1.0 修复str连接错误

--- play.py
+++ play.py
@@ -1,22 +1,19 @@
 # -*- coding: UTF-8 -*-
-from flask import Flask, url_for, render_template, request
+from flask import Flask, render_template, request
 import os
-import sys
 
 
 app = Flask(__name__)
 
-legal_suffix = ('WEBM', 'MP4', 'OGG')
+legal_suffix = ('WEBM', 'MP4')
 
 
 @app.route('/')
 def index():
     files = os.listdir('static/video')
-    video_exist = False
     for video_file in files:
         for suffix in legal_suffix:
             if suffix in video_file.split('.')[-1].upper():  # Check file in list have a suffix of webm, mp4 or ogg.
-                video_exist = True
                 return render_template('index.html', files=files)
     return render_template('404.html', error="Video directory is empty!"), 404
 
@@ -28,11 +25,8 @@ def play(video_file):
         for suffix in legal_suffix:
             if suffix in video_file.split('.')[-1].upper():
                 return render_template('player.html', user_agent=user_agent, video_file=video_file)
-                # print(video_file)
-                # return "abc"
     return render_template('404.html', error="Video file %s doesn't exist!" % video_file), 404
 
 
 if __name__ == '__main__':
-    # app.run(port=8000)
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8508)
